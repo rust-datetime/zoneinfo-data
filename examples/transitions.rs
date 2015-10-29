a1d0634b83c979155cfca4a2fab383b7ac80172e
Ben S <ogham@bsago.me>
2015-10-29 12:20:48 +0000

    Fix transitions example

--- examples/transitions.rs
+++ examples/transitions.rs
@@ -1,13 +1,9 @@
 extern crate zoneinfo_data;
 
-extern crate datetime;
-use datetime::instant::Instant;
-use datetime::local::{LocalDateTime, DatePiece, TimePiece};
-
 fn main() {
     let zone = zoneinfo_data::Africa::Tripoli;
     for transition in zone.transitions {
         println!("    Transition {{ occurs_at: {:?},   utc_offset: {}, std_offset: {} }},",
-                 transition.occurs_at, transition.utc_offset, transition.std_offset);
+                 transition.occurs_at, transition.utc_offset, transition.dst_offset);
     }
 }
